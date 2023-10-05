5ee58e3c258990c4150e527af98df5f87d3438ce
danilo-89 <danilo89abcd@gmail.com>
2023-10-05 23:00:45 +0200

    config updated with colors and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            azure: '#0085ff',
+            night: '#141414',
+            slateGray: '#696f8c',
+            white: '#ffffff',
+            whiteSmoke: '#f4f4f4',
+        },
+        extend: {
+            keyframes: {
+                'dot-1': {
+                    '0%': { opacity: '0.1' },
+                    '10%': { opacity: '0.1' },
+                    '100%': { opacity: '1' },
+                },
+                'dot-2': {
+                    '0%': { opacity: '0.05' },
+                    '30%': { opacity: '0.1' },
+                    '100%': { opacity: '1' },
+                },
+                'dot-3': {
+                    '0%': { opacity: '0' },
+                    '70%': { opacity: '0.1' },
+                    '100%': { opacity: '1' },
+                },
+            },
+        },
     },
     plugins: [],
 }
