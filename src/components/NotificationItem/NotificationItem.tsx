154273be994f5a7ebf27c4e9167e0af478dca979
danilo-89 <danilo89abcd@gmail.com>
2023-10-07 14:33:37 +0200

    refactoring

--- src/components/NotificationItem/NotificationItem.tsx
+++ src/components/NotificationItem/NotificationItem.tsx
@@ -1,16 +1,50 @@
 import { readSingleNotification } from '@/requests'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import {
+    useIsMutating,
+    useMutation,
+    useMutationState,
+    useQuery,
+    useQueryClient,
+} from '@tanstack/react-query'
 import { useState } from 'react'
+
+import avatarPlaceholderImg from '@/assets/avatar-placeholder.png'
+import Spinner from '../loaders/Spinner'
+import formatDistance from 'date-fns/formatDistance'
 
 const NotificationItem = ({ item }) => {
     const queryClient = useQueryClient()
-    const [hideDot, setHideDot] = useState(false)
 
     const { mutate } = useMutation({
         mutationKey: ['read-single-notification', item.id],
         mutationFn: () => readSingleNotification(item.id),
+        onMutate(variables) {
+            console.log({ variables })
+            queryClient.setQueriesData(
+                { queryKey: ['single-notifications-state'] },
+                (data) => {
+                    console.log(data)
+                    return {
+                        ...data,
+                        [item.id]: {
+                            pending: true,
+                        },
+                    }
+                }
+            )
+        },
         onSuccess(responseData, variables, context) {
-            setHideDot(true)
+            // queryClient.setQueriesData(
+            //     { queryKey: ['notifications'], exact: false },
+            //     (currentData) => {
+            //         console.log({ currentData })
+            //         if (currentData) {
+            //             return { ...currentData, pageParams: [1] }
+            //         }
+            //         return currentData
+            //     }
+            // )
+
             queryClient.invalidateQueries({
                 queryKey: ['notifications'],
                 refetchType: 'none',
@@ -23,27 +57,77 @@ const NotificationItem = ({ item }) => {
             })
 
             console.log(responseData)
+
+            console.log('still executes')
             // queryClient.resetQueries({ queryKey: ['notifications'] })
             // setShowUnread(false)
             // // queryClient.removeQueries({ queryKey: ['notifications'] })
         },
+        onSettled(data, error, variables, context) {
+            queryClient.setQueriesData(
+                { queryKey: ['single-notifications-state'] },
+                (stateData) => {
+                    console.log({ data })
+                    console.log({ stateData })
+                    return {
+                        ...stateData,
+                        [item.id]: {
+                            pending: false,
+                            seen: data?.data?.seen || false,
+                        },
+                    }
+                }
+            )
+        },
     })
 
+    // console.log(queryClient.getQueryState(['single-notifications-state']))
+    const state = queryClient.getQueryData(['single-notifications-state'])
+    const itemState = state?.[item.id] || {}
+
+    // queryClient.getQueryState(['single-notifications-state'])
+    // queryClient.getQueryData(['single-notifications-state'])
+    // const data = useMutationState({
+    //     filters: { mutationKey: ['read-single-notification', item.id] },
+    //     select: (mutation) => mutation.state.data,
+    // })
+
+    // const isMutatingPosts = useIsMutating({
+    //     mutationKey: ['read-single-notification', item.id],
+    // })
+
+    // console.log('ms', data)
+    // console.log('isMutatingPosts', isMutatingPosts)
+
     return (
-        <li className="flex py-4 pl-6 pr-4 border text-xs z-10 relative">
+        <li className="relative z-10 flex border-b border-[#DCDEE4]/50 py-4 pl-6 pr-4 text-xs">
             {item.user ? (
-                <div className="w-10 h-10 rounded-full bg-orange-300 flex-shrink-0 mr-2"></div>
+                <img
+                    className="bg-orange-300 mr-3 h-[2.125rem] w-[2.125rem] flex-shrink-0 rounded-full"
+                    src={avatarPlaceholderImg}
+                    alt="avatar placeholder"
+                />
             ) : null}
-            <div>
-                <div>{item.body}</div>
-                <div>{item.createdAt}</div>
+            <div className="text-slateGray">
+                <div className="mb-1">{item.body}</div>
+                <div>
+                    {formatDistance(item.createdAt, new Date(), {
+                        addSuffix: true,
+                    })}
+                </div>
             </div>
             <div className="ml-auto">
-                {!item.seen && !hideDot ? (
-                    <button
-                        onClick={() => mutate()}
-                        className="inline-block h-3.5 w-3.5 bg-blue-600 rounded-full ml-2"
-                    />
+                {!item.seen && !itemState.seen ? (
+                    <>
+                        {itemState.pending ? (
+                            <Spinner />
+                        ) : (
+                            <button
+                                onClick={() => mutate()}
+                                className="ml-2 inline-block h-3.5 w-3.5 rounded-full bg-azure"
+                            />
+                        )}
+                    </>
                 ) : null}
             </div>
         </li>
