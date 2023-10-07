82df0c92f618c12a2314eed8d277362170efbfa2
danilo-89 <danilo89abcd@gmail.com>
2023-10-07 14:32:50 +0200

    added toaster provider and query error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+    QueryCache,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster, toast } from 'sonner'
 
+// Styles
 import './App.css'
 
 // Components
 import Home from '@/pages'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+        onError: (error) =>
+            toast.error(`Something went wrong: ${error.message}`),
+    }),
+})
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
+            <Toaster />
             <Home />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
