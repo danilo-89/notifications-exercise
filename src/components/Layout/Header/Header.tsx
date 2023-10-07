6cabb6dd0dcbd8a1ec01d464fec0423ef0f70b27
danilo-89 <danilo89abcd@gmail.com>
2023-10-07 14:33:19 +0200

    refactoring

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,8 +1,12 @@
-import NotificationsList from '@/components/NotificationsList'
-import { useQuery } from '@tanstack/react-query'
+import NotificationsList from '@/components/NotificationsWrapper'
+import BellIcon from '@/components/icons/BellIcon'
+import LoaderDots from '@/components/loaders/LoaderDots'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import clsx from 'clsx'
 import { useState } from 'react'
 
 const Header = () => {
+    const queryClient = useQueryClient()
     const [showNotifications, setShowNotifications] = useState(false)
 
     const {
@@ -37,21 +41,64 @@ const Header = () => {
         staleTime: Infinity,
     })
 
-    console.log(data)
+    const nt = queryClient.getQueryState(['notifications', null])
+
+    console.log({ nt })
+
+    const unseenCount = data?.unseen
+    const totalCount = data?.all
 
     return (
         <>
-            <header className="fixed flex justify-between w-full z-10 h-[4.5rem] bg-slate-100 items-center px-7">
-                <div className="w-[6.25rem] h-10 flex items-center justify-center border-2 rounded-[0.625rem]">
-                    {data.all}
+            <header className="fixed z-10 flex h-[4.5rem] w-full items-center justify-between bg-whiteSmoke px-7">
+                <div className="flex h-10 w-[6.25rem] items-center justify-center rounded-[0.625rem] border-2 border-slateGray bg-white text-slateGray">
+                    {totalCount === undefined ? <LoaderDots /> : totalCount}
                 </div>
-                <button onClick={() => setShowNotifications((curr) => !curr)}>
-                    bell icon, {data.unseen}
+
+                <button
+                    className="relative flex h-12 w-12 items-center justify-center"
+                    onClick={() => {
+                        if (!showNotifications) {
+                            if (nt?.isInvalidated) {
+                                queryClient.removeQueries({
+                                    queryKey: ['notifications'],
+                                    exact: false,
+                                })
+                            }
+                        }
+
+                        setShowNotifications((curr) => !curr)
+                    }}
+                >
+                    <BellIcon />
+                    <UnreadIndicator unseenCount={unseenCount} />
                 </button>
             </header>
             {showNotifications ? <NotificationsList /> : null}
         </>
     )
+}
+
+const UnreadIndicator = ({ unseenCount }) => {
+    if (
+        unseenCount === undefined ||
+        (typeof unseenCount === 'string' && +unseenCount > 0)
+    ) {
+        return (
+            <span
+                className={clsx(
+                    'absolute right-0 top-0 flex h-6 w-6 items-center justify-center rounded-full',
+                    unseenCount === undefined
+                        ? 'bg-[#d9d9d9] text-slateGray text-sm'
+                        : 'bg-azure text-white text-xs'
+                )}
+            >
+                {unseenCount}
+            </span>
+        )
+    }
+
+    return null
 }
 
 export default Header
