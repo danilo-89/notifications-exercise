8127a25eea349ed2f509409077003739ddcd131e
danilo-89 <danilo89abcd@gmail.com>
2023-10-27 14:37:14 +0200

    context created

---
+++ src/contexts/NotificationMutationContext.tsx
@@ -0,0 +1,74 @@
+import { createContext, ReactNode } from 'react'
+import {
+    UseMutationResult,
+    useMutation,
+    useQueryClient,
+} from '@tanstack/react-query'
+
+// Requests
+import { readSingleNotification } from '@/requests'
+
+// Types
+import { Notification } from '@/types/api'
+
+type IProps = null | UseMutationResult<
+    {
+        data: any
+        counts: {
+            all: string | undefined
+            unseen: string | undefined
+        }
+    },
+    Error,
+    string,
+    void
+>
+
+export const NotificationMutationContext = createContext<IProps>(null)
+
+export function NotificationMutationContextProvider({
+    children,
+}: {
+    children: ReactNode
+}) {
+    const queryClient = useQueryClient()
+
+    const readSingleMutation = useMutation({
+        mutationKey: ['read-single-notification'],
+        mutationFn: (id: string) => readSingleNotification(id),
+        onSuccess(responseData) {
+            // invalidate query for active notifications query
+            queryClient.invalidateQueries({
+                queryKey: ['notifications'],
+                exact: false,
+                type: 'active',
+                refetchType: 'none',
+            })
+
+            // clear data cache for inactive notifications query
+            queryClient.removeQueries({
+                queryKey: ['notifications'],
+                exact: false,
+                type: 'inactive',
+            })
+
+            // set counts based on response
+            queryClient.setQueryData(['counts'], () => {
+                const counts = responseData?.counts
+
+                return (
+                    { ...counts } || {
+                        all: undefined,
+                        unseen: undefined,
+                    }
+                )
+            })
+        },
+    })
+
+    return (
+        <NotificationMutationContext.Provider value={readSingleMutation}>
+            {children}
+        </NotificationMutationContext.Provider>
+    )
+}
